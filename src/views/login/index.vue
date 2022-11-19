cba6fe3d91ca859974e435ec472235ba3bfc1e45
冷澄峰 <2362919153@qq.com>
2022-11-19 15:05:31 +0800

    fixed

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="login-page">
-    <div class="left"></div>
+    <div class="left">
+      <div class="bg">
+        <img src="@/assets/img/login-box-bg.svg" alt="" style="width: 400px" />
+        <div class="welcome">欢迎使用本系统</div>
+      </div>
+    </div>
     <div class="right">
       <el-card class="box-card">
         <template #header>
@@ -23,19 +28,36 @@ import LoginAccount from './components/loginAccount.vue'
   height: 100vh;
   display: flex;
   .left {
-    background: url(@/assets/img/bgc.svg);
+    position: relative;
+    background: #48525c;
+    background-size: 100% 100%;
     background-position: 100% 100%;
-    width: 35%;
+    width: 50%;
+    .bg {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .welcome {
+      color: #fff;
+      font-size: 20px;
+      text-align: center;
+    }
   }
   .right {
-    width: 65%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
     background: url(@/assets/img/bgc.svg);
     background-position: 100% 100%;
     display: flex;
     align-items: center;
     .box-card {
       min-width: 400px;
-      width: 40%;
+      height: 400px;
+      width: 60%;
       .card-header {
         text-align: center;
       }
